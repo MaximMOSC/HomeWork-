c77825aa25c6c43bee683e24fb54e2654f19c482
MaximPetrakov <petrakovmaxim@mail.ru>
2023-10-22 20:11:53 +0300

    Task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// Не использовать строки для расчета.
+
+int ReadInt(string msg)//Объявление функции.Функция ReadInt , параметр msg.
+{
+    System.Console.WriteLine(msg);  // Выводит на экран параметр msg
+    string Num = Console.ReadLine(); //Функция считывания строки с консоли
+    int Number;//Объявление переменной.
+    Number = int.Parse(Num);//Как результат - преобразование строки в число
+    return Number;//Возврат из функции
+}
+int Number = ReadInt("Введите трёхзначное число");
+if (Number < 1000 && Number > 99)
+{
+    int secondDigit = (Number % 100 - Number % 10) / 10;
+    System.Console.WriteLine(" Вторая цифра " + secondDigit );
+}
+else System.Console.WriteLine("Это число не трёхзначное");
---
+++ Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+int ReadInt(string msg)//Объявление функции.Функция ReadInt , параметр msg.
+{
+    System.Console.WriteLine(msg);  // Выводит на экран параметр msg
+    string Num = Console.ReadLine(); //Функция считывания строки с консоли
+    int Number;//Объявление переменной.
+    Number = int.Parse(Num);//Как результат - преобразование строки в число
+    return Number;//Возврат из функции
+}
+int DayNumber = ReadInt("Введите цифру дня недели");
+if (DayNumber == 6 || DayNumber == 7)
+{
+    System.Console.WriteLine("Это выходной день");
+}
+else if (DayNumber < 6 && DayNumber > 0)
+{
+    System.Console.WriteLine("Это не выходной день");
+}
+
+
